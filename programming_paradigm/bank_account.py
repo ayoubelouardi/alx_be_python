e4dd83b55c67a62fbcdfc375ebbfb95f985d6a95
katana <you@example.com>
2026-01-03 18:59:25 +0100

    Programming Paradigms & Exception handling

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,24 +1,24 @@
 # bank_account.py
 
 class BankAccount:
-    def __init__(self, account_balance=0):
+    def __init__(self, account_balance=0.00):
         # Initialize account balance, ensuring it's not negative
-        if account_balance >= 0:
+        if account_balance >= 0.00:
             self.__account_balance = account_balance
         else:
             print("Account Balance cannot be negative!")
-            self.__account_balance = 0
+            self.__account_balance = 0.00
 
     def deposit(self, amount):
         """Add money to the account balance."""
-        if amount > 0:
+        if amount > 0.00:
             self.__account_balance += amount
         else:
             print("Amount must be positive")
 
     def withdraw(self, amount):
         """Withdraw money if sufficient funds exist."""
-        if amount > 0:
+        if amount > 0.00:
             if self.__account_balance >= amount:
                 self.__account_balance -= amount
                 return True
